5dbfb2f77265fbe7dbc1bc33afb8ad3296b40e0d
Dhriti Soni <dhritisoni4@gmail.com>
2026-08-02 22:52:39 +0530

    feat(error): wrap app with global error boundary

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ErrorBoundary from "./components/error/ErrorBoundary";
+
 import "./index.css";
 
 import App from "./App";
 
+import ErrorBoundary from "./components/error/ErrorBoundary";
 import { CommandProvider } from "./components/command/CommandProvider";
 import { ToastProvider } from "./components/ui/toast/ToastProvider";
 import { SettingsProvider } from "./components/Settings/SettingsProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CommandProvider>
-      <ToastProvider>
-        <SettingsProvider>
-          <App />
-        </SettingsProvider>
-      </ToastProvider>
-    </CommandProvider>
+    <ErrorBoundary>
+      <CommandProvider>
+        <ToastProvider>
+          <SettingsProvider>
+            <App />
+          </SettingsProvider>
+        </ToastProvider>
+      </CommandProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
